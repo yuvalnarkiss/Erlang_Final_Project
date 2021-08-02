a6b1e8426007f36e8f47b8de8c09f6e81d00429c
KatrinNekhin <katrinn@post.bgu.ac.il>
2021-08-03 00:47:32 +0300

    initial server

---
+++ src/server.erl
@@ -0,0 +1,134 @@
+%%%-------------------------------------------------------------------
+%%% @author katrinn
+%%% @copyright (C) 2021, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 02. Aug 2021 22:45
+%%%-------------------------------------------------------------------
+-module(server).
+-author("katrinn").
+
+-behaviour(gen_server).
+
+%% API
+-export([start_link/0]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
+  code_change/3]).
+
+-define(SERVER, ?MODULE).
+
+-record(server_state, {}).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%% @doc Spawns the server and registers the local name (unique)
+-spec(start_link() ->
+  {ok, Pid :: pid()} | ignore | {error, Reason :: term()}).
+start_link() ->
+  gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%% @private
+%% @doc Initializes the server
+-spec(init(Args :: term()) ->
+  {ok, State :: #server_state{}} | {ok, State :: #server_state{}, timeout() | hibernate} |
+  {stop, Reason :: term()} | ignore).
+init([]) ->
+  Num_of_sensors = random:uniform(571) + 5, % number of sensors randomized between 6 - 576
+  [Comp_Pos | Pos_list] = randomize_positions(Num_of_sensors,0,0), %ToDo: offsets are temp
+  Fixed_Comp_PID = fixed_comp_pid, %ToDo: add function that creates a fixed computer and returns pid
+  Radius = find_radius([Comp_Pos | Pos_list]),
+  Sensor_PID_Pos_list = create_sensors(Pos_list),
+  %ToDo: call a function that sends <Sensor_PID_list> to the main_PC and waits for full tree.
+  % The next psudo function represents what needs to be implemented in main_pc
+  % G = new(),
+  % add vertices to G, each labeled after sensor_pid + pos (and one comp_pid)
+  % for each couple of vertices -> if dist(P1,P2) < Radius, add edge between them
+  {ok, #server_state{}}.
+
+%% @private
+%% @doc Handling call messages
+-spec(handle_call(Request :: term(), From :: {pid(), Tag :: term()},
+    State :: #server_state{}) ->
+  {reply, Reply :: term(), NewState :: #server_state{}} |
+  {reply, Reply :: term(), NewState :: #server_state{}, timeout() | hibernate} |
+  {noreply, NewState :: #server_state{}} |
+  {noreply, NewState :: #server_state{}, timeout() | hibernate} |
+  {stop, Reason :: term(), Reply :: term(), NewState :: #server_state{}} |
+  {stop, Reason :: term(), NewState :: #server_state{}}).
+handle_call(_Request, _From, State = #server_state{}) ->
+  {reply, ok, State}.
+
+%% @private
+%% @doc Handling cast messages
+-spec(handle_cast(Request :: term(), State :: #server_state{}) ->
+  {noreply, NewState :: #server_state{}} |
+  {noreply, NewState :: #server_state{}, timeout() | hibernate} |
+  {stop, Reason :: term(), NewState :: #server_state{}}).
+handle_cast(_Request, State = #server_state{}) ->
+  {noreply, State}.
+
+%% @private
+%% @doc Handling all non call/cast messages
+-spec(handle_info(Info :: timeout() | term(), State :: #server_state{}) ->
+  {noreply, NewState :: #server_state{}} |
+  {noreply, NewState :: #server_state{}, timeout() | hibernate} |
+  {stop, Reason :: term(), NewState :: #server_state{}}).
+handle_info(_Info, State = #server_state{}) ->
+  {noreply, State}.
+
+%% @private
+%% @doc This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+-spec(terminate(Reason :: (normal | shutdown | {shutdown, term()} | term()),
+    State :: #server_state{}) -> term()).
+terminate(_Reason, _State = #server_state{}) ->
+  ok.
+
+%% @private
+%% @doc Convert process state when code is changed
+-spec(code_change(OldVsn :: term() | {down, term()}, State :: #server_state{},
+    Extra :: term()) ->
+  {ok, NewState :: #server_state{}} | {error, Reason :: term()}).
+code_change(_OldVsn, State = #server_state{}, _Extra) ->
+  {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+randomize_positions(Num_of_sensors,OffsetX,OffsetY) ->
+  randomize_positions([],Num_of_sensors,OffsetX,OffsetY).
+
+randomize_positions(Pos_List,0,_OffsetX,_OffsetY) ->
+  % get rid of duplicates
+  Pos_Set = sets:from_list(Pos_List),
+  sets:to_list(Pos_Set).
+randomize_positions(Pos_List,Num_of_sensors,OffsetX,OffsetY) ->
+  X = 20 * (random:uniform(24) - 1) + OffsetX,
+  Y = 20 * (random:uniform(24) - 1) + OffsetX,
+  randomize_positions([{X,Y} | Pos_List],Num_of_sensors-1,OffsetX,OffsetY).
+
+create_sensors([]) -> [];
+  create_sensors([Position|Pos_list]) ->
+  {ok, Sensor_PID} = sensor:start(Position),
+  [{Sensor_PID, Position} | create_sensors(Pos_list)].
+
+find_radius([{_X,_Y}]) -> 0;
+find_radius([{X1,Y1}| Pos_list]) ->
+  Dist = [ dist({X1,Y1},{X2,Y2}) || {X2, Y2} <- Pos_list],
+  Min_dist = lists:min(Dist),
+  Min_Radius = lists:max([ Min_dist | find_radius(Pos_list) ]),
+  Min_Radius.
+
+dist({X1,Y1},{X2,Y2}) -> trunc(math:ceil(math:sqrt(math:pow(X2 - X1, 2) + math:pow(Y2 - Y1, 2)))).
