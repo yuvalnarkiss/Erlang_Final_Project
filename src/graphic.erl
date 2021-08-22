793b9a8dd3c47f67469f61fd6be2a6957363b9ef
ubuntu <yuvalnar@post.bgu.ac.il>
2021-08-23 01:18:10 +0300

    FINISHED!!!

--- src/graphic.erl
+++ src/graphic.erl
@@ -62,118 +62,123 @@ init([]) ->
   % Initialize Values
   ets:insert(status_db,[{q1,?INIT_VAL},{q3,?INIT_VAL},{q2,?INIT_VAL},{q4,?INIT_VAL},{whole,?INIT_VAL},{self_temp,0.0},{stat_avg,{0.0,0.0,0.0,0.0}}]),
 
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+
   %Graphics
   WxServer = wx:new(),
-  Frame = wxFrame:new(WxServer, ?wxID_ANY, "MAP", [{size,?SCREEN_SIZE}]),
-  BackGroundIMG0 = wxImage:new("background.png"),
-  BackGroundIMG = wxImage:scale(BackGroundIMG0,?SCREEN_WIDTH,?SCREEN_HEIGHT),
-  {SC_X,SC_Y} = {trunc(?STATIONARY_COMP_X/?AREA_WIDTH_METER * ?MAP_WIDTH),trunc(?STATIONARY_COMP_Y/?AREA_HEIGHT_METER * ?MAP_HEIGHT)},
-  Panel  = wxPanel:new(Frame),
+  Frame = wxFrame:new(WxServer, ?wxID_ANY, "MAP", [{pos, ?wxDefaultPosition},{size, ?wxDefaultSize}]),
+  {SC_X,SC_Y} = {?STATIONARY_COMP_X,?STATIONARY_COMP_Y},
+  Panel  = wxPanel:new(Frame,[{size, {?MAP_WIDTH, ?MAP_HEIGHT}}]),
+  Panel2  = wxPanel:new(Frame, [{size, {?STATISITC_WIDTH,?SCREEN_HEIGHT}}]),
+  wxPanel:setBackgroundColour(Panel2, {169, 169, 169}),
 
   %Statistics box-------------------------------------------------------------------
   MainSizer = wxBoxSizer:new(?wxHORIZONTAL),
   Font = wxFont:new(12,?wxDECORATIVE,?wxFONTSTYLE_NORMAL,?wxFONTWEIGHT_LIGHT,[]),
 
-  Title1 = wxStaticText:new(Panel, ?wxID_ANY, "Main Statistics", [{style, ?wxALIGN_CENTER}]),
+  Title1 = wxStaticText:new(Panel2, ?wxID_ANY, "Main Statistics", [{style, ?wxALIGN_CENTER}]),
   wxTextCtrl:setFont(Title1, Font),
 
-  Title2 = wxStaticText:new(Panel, ?wxID_ANY, "Upper Left Quarter Statistics", [{style, ?wxALIGN_CENTER}]),
+  Title2 = wxStaticText:new(Panel2, ?wxID_ANY, "Upper Left Quarter Statistics", [{style, ?wxALIGN_CENTER}]),
   wxTextCtrl:setFont(Title2, Font),
 
-  Title3 = wxStaticText:new(Panel, ?wxID_ANY, "Upper Right Quarter Statistics", [{style, ?wxALIGN_CENTER}]),
+  Title3 = wxStaticText:new(Panel2, ?wxID_ANY, "Upper Right Quarter Statistics", [{style, ?wxALIGN_CENTER}]),
   wxTextCtrl:setFont(Title3, Font),
 
-  Title4 = wxStaticText:new(Panel, ?wxID_ANY, "Lower Left Quarter Statistics", [{style, ?wxALIGN_CENTER}]),
+  Title4 = wxStaticText:new(Panel2, ?wxID_ANY, "Lower Left Quarter Statistics", [{style, ?wxALIGN_CENTER}]),
   wxTextCtrl:setFont(Title4, Font),
 
-  Title5 = wxStaticText:new(Panel, ?wxID_ANY, "Lower Right Quarter Statistics", [{style, ?wxALIGN_CENTER}]),
+  Title5 = wxStaticText:new(Panel2, ?wxID_ANY, "Lower Right Quarter Statistics", [{style, ?wxALIGN_CENTER}]),
   wxTextCtrl:setFont(Title5, Font),
 
-  Title6 = wxStaticText:new(Panel, ?wxID_ANY, "Sensor Condition", [{style, ?wxALIGN_CENTER}]),
+  Title6 = wxStaticText:new(Panel2, ?wxID_ANY, "Sensor Condition", [{style, ?wxALIGN_CENTER}]),
   wxTextCtrl:setFont(Title6, Font),
 
-  AVG_Temp1 = wxStaticText:new(Panel, ?wxID_ANY, "Average Temp:", [{style, ?wxALIGN_CENTER}]),
-  AVG_Temp2 = wxStaticText:new(Panel, ?wxID_ANY, "Average Temp:", [{style, ?wxALIGN_CENTER}]),
-  AVG_Temp3 = wxStaticText:new(Panel, ?wxID_ANY, "Average Temp:", [{style, ?wxALIGN_CENTER}]),
-  AVG_Temp4 = wxStaticText:new(Panel, ?wxID_ANY, "Average Temp:", [{style, ?wxALIGN_CENTER}]),
-  AVG_Temp5 = wxStaticText:new(Panel, ?wxID_ANY, "Average Temp:", [{style, ?wxALIGN_CENTER}]),
+  AVG_Temp1 = wxStaticText:new(Panel2, ?wxID_ANY, "Average Temp:", [{style, ?wxALIGN_CENTER}]),
+  AVG_Temp2 = wxStaticText:new(Panel2, ?wxID_ANY, "Average Temp:", [{style, ?wxALIGN_CENTER}]),
+  AVG_Temp3 = wxStaticText:new(Panel2, ?wxID_ANY, "Average Temp:", [{style, ?wxALIGN_CENTER}]),
+  AVG_Temp4 = wxStaticText:new(Panel2, ?wxID_ANY, "Average Temp:", [{style, ?wxALIGN_CENTER}]),
+  AVG_Temp5 = wxStaticText:new(Panel2, ?wxID_ANY, "Average Temp:", [{style, ?wxALIGN_CENTER}]),
 
   wxTextCtrl:setFont(AVG_Temp1, Font),
   wxTextCtrl:setFont(AVG_Temp2, Font),
   wxTextCtrl:setFont(AVG_Temp3, Font),
   wxTextCtrl:setFont(AVG_Temp4, Font),
   wxTextCtrl:setFont(AVG_Temp5, Font),
-  AVG_Temp_TXT1 = wxTextCtrl:new(Panel, ?wxID_ANY, [{value, "***************"}, {style, ?wxTE_RIGHT}]),
+  AVG_Temp_TXT1 = wxTextCtrl:new(Panel2, ?wxID_ANY, [{value, "***************"}, {style, ?wxTE_RIGHT}]),
   wxTextCtrl:setFont(AVG_Temp_TXT1, Font),
   wxTextCtrl:setEditable(AVG_Temp_TXT1, false),
-  AVG_Temp_TXT2 = wxTextCtrl:new(Panel, ?wxID_ANY, [{value, "***************"}, {style, ?wxTE_RIGHT}]),
+  AVG_Temp_TXT2 = wxTextCtrl:new(Panel2, ?wxID_ANY, [{value, "***************"}, {style, ?wxTE_RIGHT}]),
   wxTextCtrl:setFont(AVG_Temp_TXT2, Font),
   wxTextCtrl:setEditable(AVG_Temp_TXT2, false),
-  AVG_Temp_TXT3 = wxTextCtrl:new(Panel, ?wxID_ANY, [{value, "***************"}, {style, ?wxTE_RIGHT}]),
+  AVG_Temp_TXT3 = wxTextCtrl:new(Panel2, ?wxID_ANY, [{value, "***************"}, {style, ?wxTE_RIGHT}]),
   wxTextCtrl:setFont(AVG_Temp_TXT3, Font),
   wxTextCtrl:setEditable(AVG_Temp_TXT3, false),
-  AVG_Temp_TXT4 = wxTextCtrl:new(Panel, ?wxID_ANY, [{value, "***************"}, {style, ?wxTE_RIGHT}]),
+  AVG_Temp_TXT4 = wxTextCtrl:new(Panel2, ?wxID_ANY, [{value, "***************"}, {style, ?wxTE_RIGHT}]),
   wxTextCtrl:setFont(AVG_Temp_TXT4, Font),
   wxTextCtrl:setEditable(AVG_Temp_TXT4, false),
-  AVG_Temp_TXT5 = wxTextCtrl:new(Panel, ?wxID_ANY, [{value, "***************"}, {style, ?wxTE_RIGHT}]),
+  AVG_Temp_TXT5 = wxTextCtrl:new(Panel2, ?wxID_ANY, [{value, "***************"}, {style, ?wxTE_RIGHT}]),
   wxTextCtrl:setFont(AVG_Temp_TXT5, Font),
   wxTextCtrl:setEditable(AVG_Temp_TXT5, false),
 
-  AVG_Hum1 = wxStaticText:new(Panel, ?wxID_ANY, "Average Humidity:", [{style, ?wxALIGN_CENTER}]),
-  AVG_Hum2 = wxStaticText:new(Panel, ?wxID_ANY, "Average Humidity:", [{style, ?wxALIGN_CENTER}]),
-  AVG_Hum3 = wxStaticText:new(Panel, ?wxID_ANY, "Average Humidity:", [{style, ?wxALIGN_CENTER}]),
-  AVG_Hum4 = wxStaticText:new(Panel, ?wxID_ANY, "Average Humidity:", [{style, ?wxALIGN_CENTER}]),
-  AVG_Hum5 = wxStaticText:new(Panel, ?wxID_ANY, "Average Humidity:", [{style, ?wxALIGN_CENTER}]),
+  AVG_Hum1 = wxStaticText:new(Panel2, ?wxID_ANY, "Average Humidity:", [{style, ?wxALIGN_CENTER}]),
+  AVG_Hum2 = wxStaticText:new(Panel2, ?wxID_ANY, "Average Humidity:", [{style, ?wxALIGN_CENTER}]),
+  AVG_Hum3 = wxStaticText:new(Panel2, ?wxID_ANY, "Average Humidity:", [{style, ?wxALIGN_CENTER}]),
+  AVG_Hum4 = wxStaticText:new(Panel2, ?wxID_ANY, "Average Humidity:", [{style, ?wxALIGN_CENTER}]),
+  AVG_Hum5 = wxStaticText:new(Panel2, ?wxID_ANY, "Average Humidity:", [{style, ?wxALIGN_CENTER}]),
 
   wxTextCtrl:setFont(AVG_Hum1, Font),
   wxTextCtrl:setFont(AVG_Hum2, Font),
   wxTextCtrl:setFont(AVG_Hum3, Font),
   wxTextCtrl:setFont(AVG_Hum4, Font),
   wxTextCtrl:setFont(AVG_Hum5, Font),
-  AVG_Hum_TXT1 = wxTextCtrl:new(Panel, ?wxID_ANY, [{value, "***************"}, {style, ?wxTE_RIGHT}]),
+  AVG_Hum_TXT1 = wxTextCtrl:new(Panel2, ?wxID_ANY, [{value, "***************"}, {style, ?wxTE_RIGHT}]),
   wxTextCtrl:setFont(AVG_Hum_TXT1, Font),
   wxTextCtrl:setEditable(AVG_Hum_TXT1, false),
-  AVG_Hum_TXT2 = wxTextCtrl:new(Panel, ?wxID_ANY, [{value, "***************"}, {style, ?wxTE_RIGHT}]),
+  AVG_Hum_TXT2 = wxTextCtrl:new(Panel2, ?wxID_ANY, [{value, "***************"}, {style, ?wxTE_RIGHT}]),
   wxTextCtrl:setFont(AVG_Hum_TXT2, Font),
   wxTextCtrl:setEditable(AVG_Hum_TXT2, false),
-  AVG_Hum_TXT3 = wxTextCtrl:new(Panel, ?wxID_ANY, [{value, "***************"}, {style, ?wxTE_RIGHT}]),
+  AVG_Hum_TXT3 = wxTextCtrl:new(Panel2, ?wxID_ANY, [{value, "***************"}, {style, ?wxTE_RIGHT}]),
   wxTextCtrl:setFont(AVG_Hum_TXT3, Font),
   wxTextCtrl:setEditable(AVG_Hum_TXT3, false),
-  AVG_Hum_TXT4 = wxTextCtrl:new(Panel, ?wxID_ANY, [{value, "***************"}, {style, ?wxTE_RIGHT}]),
+  AVG_Hum_TXT4 = wxTextCtrl:new(Panel2, ?wxID_ANY, [{value, "***************"}, {style, ?wxTE_RIGHT}]),
   wxTextCtrl:setFont(AVG_Hum_TXT4, Font),
   wxTextCtrl:setEditable(AVG_Hum_TXT4, false),
-  AVG_Hum_TXT5 = wxTextCtrl:new(Panel, ?wxID_ANY, [{value, "***************"}, {style, ?wxTE_RIGHT}]),
+  AVG_Hum_TXT5 = wxTextCtrl:new(Panel2, ?wxID_ANY, [{value, "***************"}, {style, ?wxTE_RIGHT}]),
   wxTextCtrl:setFont(AVG_Hum_TXT5, Font),
   wxTextCtrl:setEditable(AVG_Hum_TXT5, false),
 
 
-  Sens_Temp = wxStaticText:new(Panel, ?wxID_ANY, "Average Sensor's Temp:", [{style, ?wxALIGN_CENTER}]),
+  Sens_Temp = wxStaticText:new(Panel2, ?wxID_ANY, "Average Sensor's Temp:", [{style, ?wxALIGN_CENTER}]),
   wxTextCtrl:setFont(Sens_Temp, Font),
-  Sens_Temp_TXT = wxTextCtrl:new(Panel, ?wxID_ANY, [{value, "***************"}, {style, ?wxTE_RIGHT}]),
+  Sens_Temp_TXT = wxTextCtrl:new(Panel2, ?wxID_ANY, [{value, "***************"}, {style, ?wxTE_RIGHT}]),
   wxTextCtrl:setFont(Sens_Temp_TXT, Font),
   wxTextCtrl:setEditable(Sens_Temp_TXT, false),
 
-  Btry_lvl = wxStaticText:new(Panel, ?wxID_ANY, "Average Battery Level:", [{style, ?wxALIGN_CENTER}]),
+  Btry_lvl = wxStaticText:new(Panel2, ?wxID_ANY, "Average Battery Level:", [{style, ?wxALIGN_CENTER}]),
   wxTextCtrl:setFont(Btry_lvl, Font),
-  Btry_lvl_TXT = wxTextCtrl:new(Panel, ?wxID_ANY, [{value, "***************"}, {style, ?wxTE_RIGHT}]),
+  Btry_lvl_TXT = wxTextCtrl:new(Panel2, ?wxID_ANY, [{value, "***************"}, {style, ?wxTE_RIGHT}]),
   wxTextCtrl:setFont(Btry_lvl_TXT, Font),
   wxTextCtrl:setEditable(Btry_lvl_TXT, false),
 
-  Btry_SD = wxStaticText:new(Panel, ?wxID_ANY, "Battery Standard Deviation:", [{style, ?wxALIGN_CENTER}]),
+  Btry_SD = wxStaticText:new(Panel2, ?wxID_ANY, "Battery Standard Deviation:", [{style, ?wxALIGN_CENTER}]),
   wxTextCtrl:setFont(Btry_SD, Font),
-  Btry_SD_TXT = wxTextCtrl:new(Panel, ?wxID_ANY, [{value, "***************"}, {style, ?wxTE_RIGHT}]),
+  Btry_SD_TXT = wxTextCtrl:new(Panel2, ?wxID_ANY, [{value, "***************"}, {style, ?wxTE_RIGHT}]),
   wxTextCtrl:setFont(Btry_SD_TXT, Font),
   wxTextCtrl:setEditable(Btry_SD_TXT, false),
 
-  AVG_MSG = wxStaticText:new(Panel, ?wxID_ANY, "Average No. of messages:", [{style, ?wxALIGN_CENTER}]),
+  AVG_MSG = wxStaticText:new(Panel2, ?wxID_ANY, "Average No. of messages:", [{style, ?wxALIGN_CENTER}]),
   wxTextCtrl:setFont(AVG_MSG, Font),
-  AVG_MSG_TXT = wxTextCtrl:new(Panel, ?wxID_ANY, [{value, "***************"}, {style, ?wxTE_RIGHT}]),
+  AVG_MSG_TXT = wxTextCtrl:new(Panel2, ?wxID_ANY, [{value, "***************"}, {style, ?wxTE_RIGHT}]),
   wxTextCtrl:setFont(AVG_MSG_TXT, Font),
   wxTextCtrl:setEditable(AVG_MSG_TXT, false),
 
-  MSG_SD = wxStaticText:new(Panel, ?wxID_ANY, "No. of sent messages Standard Deviation:", [{style, ?wxALIGN_CENTER}]),
+  MSG_SD = wxStaticText:new(Panel2, ?wxID_ANY, "No. of sent messages Standard Deviation:", [{style, ?wxALIGN_CENTER}]),
   wxTextCtrl:setFont(MSG_SD, Font),
-  MSG_SD_TXT = wxTextCtrl:new(Panel, ?wxID_ANY, [{value, "***************"}, {style, ?wxTE_RIGHT}]),
+  MSG_SD_TXT = wxTextCtrl:new(Panel2, ?wxID_ANY, [{value, "***************"}, {style, ?wxTE_RIGHT}]),
   wxTextCtrl:setFont(MSG_SD_TXT, Font),
   wxTextCtrl:setEditable(MSG_SD_TXT, false),
 
@@ -261,22 +266,26 @@ init([]) ->
   wxSizer:add(StatisticsSizer, LineAVGMsg, [{flag, ?wxEXPAND}]),
   wxSizer:add(StatisticsSizer, LineMsgSD, [{flag, ?wxEXPAND}]),
 
-  wxSizer:add(MainSizer, StatisticsSizer,    [{proportion,1},{flag, ?wxALIGN_RIGHT}, {border, 5}]),
 
-  wxWindow:setSizer(Frame, MainSizer),
+  wxSizer:add(MainSizer, Panel, [{proportion,2},{flag, ?wxEXPAND bor ?wxALIGN_LEFT}]),
+  wxSizer:add(MainSizer, Panel2, [{proportion,1},{flag, ?wxEXPAND bor ?wxALL}]),
 
-  Vbox = wxBoxSizer:new(?wxVERTICAL),
-  wxSizer:add(Vbox, Panel, [{flag, ?wxEXPAND}]),
+
+
+  wxFrame:setSizerAndFit(Frame, MainSizer),
+  wxFrame:setSizerAndFit(Panel2, StatisticsSizer),
+  wxFrame:show(Frame),
 
   wxFrame:show(Frame),
   CallBackPaint =	fun(#wx{event = #wxPaint{}}, _wxObj)->
     Paint = wxBufferedPaintDC:new(Panel),
+    BackGroundIMG0 = wxImage:new("background.png"),
+    BackGroundIMG = wxImage:scale(BackGroundIMG0,?MAP_WIDTH,?MAP_HEIGHT),
     BackGround = wxBitmap:new(BackGroundIMG),
+    wxImage:destroy(BackGroundIMG0),
+    wxImage:destroy(BackGroundIMG),
     wxDC:drawBitmap(Paint,BackGround,{0,0}),
     wxBitmap:destroy(BackGround),
-    %Statistics = wxBitmap:new("statistics.bmp"),
-    %wxDC:drawBitmap(Paint,Statistics,{735,0}),
-    %wxBitmap:destroy(Statistics),
     Stationary_comp = wxBitmap:new("stationary_comp1.bmp"),
     wxDC:drawBitmap(Paint,Stationary_comp,{SC_X,SC_Y}),
     wxBitmap:destroy(Stationary_comp),
@@ -446,40 +455,11 @@ drawETS(Painter,Key,Battery) ->
       drawETS(Painter,ets:next(screen_db,Key),Battery_State);
     _ ->
       [{{Sensor_X,Sensor_Y},SensorIMG}] = ets:lookup(screen_db,Key),
-      Pixel_Pos =  {trunc(Sensor_X/?AREA_WIDTH_METER * ?MAP_WIDTH),trunc(Sensor_Y/?AREA_HEIGHT_METER * ?MAP_HEIGHT)},
+      Pixel_Pos =  {trunc((Sensor_X/?AREA_WIDTH_METER) * ?MAP_WIDTH),trunc((Sensor_Y/?AREA_HEIGHT_METER) * ?MAP_HEIGHT)},
       SensorBMP = wxBitmap:new(SensorIMG),
       wxDC:drawBitmap(Painter, SensorBMP, Pixel_Pos),
       wxBitmap:destroy(SensorBMP),
       drawETS(Painter,ets:next(screen_db,Key),Battery)
-  end.
-
-drawTEXT(Painter) ->  drawTEXT(Painter,ets:first(status_db)).
-drawTEXT(Painter,Key) ->
-  case Key of
-    '$end_of_table' -> ok;
-    stat_avg ->
-      [{_Key, {{BatteryAVG,BatterySTD},{MsgCountAVG,MsgCountSTD}}}] = ets:lookup(status_db,Key),
-      wxDC:drawText(Painter,io_lib:format("~.3f",[BatteryAVG]) ,{1250,669}),
-      wxDC:drawText(Painter,io_lib:format("~.3f",[BatterySTD]) ,{1250,670}),
-      wxDC:drawText(Painter,io_lib:format("~.3f",[MsgCountAVG]) ,{1250,671}),
-      wxDC:drawText(Painter,io_lib:format("~.3f",[MsgCountSTD]) ,{1250,672}),
-      drawTEXT(Painter,ets:next(status_db,Key));
-    self_temp ->
-      [{_Key,SelfTemp}] = ets:lookup(status_db,Key),
-      wxDC:drawText(Painter,io_lib:format("~.3f",[SelfTemp]) ++ "  Celcius",{1250,765}),
-      drawTEXT(Painter,ets:next(status_db,Key));
-    Key ->
-      [{_Key,{TempAVG,HumidityAVG}}] = ets:lookup(status_db,Key),
-      {Pos1,Pos2} = case Key of
-                      whole -> {{1250,75},{1250,116}};
-                      q1 -> {{1250,213},{1250,254}};
-                      q2 -> {{1250,351},{1250,392}};
-                      q3 -> {{1250,489},{1250,530}};
-                      q4 -> {{1250,627},{1250,668}}
-                    end,
-      wxDC:drawText(Painter,io_lib:format("~.3f",[TempAVG]) ++ "  Celcius",Pos1),
-      wxDC:drawText(Painter,io_lib:format("~.3f",[HumidityAVG]) ++ "%",Pos2),
-      drawTEXT(Painter,ets:next(status_db,Key))
   end.
 
 state_to_img(State) ->
--- src/params.hrl
+++ src/params.hrl
@@ -8,18 +8,19 @@
 %%%-------------------------------------------------------------------
 -author("katrinn").
 
--define(STATIONARY_COMP_POS,{1495,5}).
--define(STATIONARY_COMP_X,1495).
+-define(STATIONARY_COMP_POS,{1380,5}).
+-define(STATIONARY_COMP_X,1380).
 -define(STATIONARY_COMP_Y,5).
--define(SCREEN_SIZE,{1260, 720}).
--define(SCREEN_WIDTH,1260).
--define(SCREEN_HEIGHT,720).
--define(MAP_SIZE,{1080, 720}).
--define(MAP_WIDTH,1080).
--define(MAP_HEIGHT,720).
+-define(SCREEN_SIZE,{1920, 957}).
+-define(SCREEN_WIDTH,1920).
+-define(SCREEN_HEIGHT,957).
+-define(MAP_SIZE,{1432, 957}).
+-define(MAP_WIDTH,1432).
+-define(MAP_HEIGHT,957).
 -define(AREA_SIZE_METER,{1500, 1000}).
 -define(AREA_WIDTH_METER,1500).
 -define(AREA_HEIGHT_METER,1000).
+-define(STATISITC_WIDTH,?SCREEN_WIDTH-?MAP_WIDTH).
 
 -define(ULQXBOUNDARY, 480).
 -define(ULQYBOUNDARY, 440).
